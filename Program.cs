032486923286424adf0194766c69dcf217826e87
phavryliuk <petrrrusyk93@gmail.com>
2022-10-19 19:54:21 +0300

    All selection methods is done

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 {
     private static void Main(string[] args)
     {
+        BubbleSorting();
+        SelectionSorting();
+        InsertionSorting();
 
         int arrayValue = 0;
 
@@ -56,6 +59,7 @@
                 }
 
             }
+
             for (int i = 0; i < myArray.Length; i++)
             {
                 Console.WriteLine(myArray[i]);
@@ -67,44 +71,87 @@
         }
 
 
-        //int[] SelectionSorting()
-        //{
+        int[] SelectionSorting()
+        {
 
-        //    int[] myArray = InputArray();
+            int[] myArray = InputArray();
 
-        //    Console.WriteLine("\n Sorting array with selection method  : ");
-        //    Console.WriteLine($"////////////////////////////////////////");
-        //    Console.WriteLine();
+            Console.WriteLine("\n Sorting array with selection method  : ");
+            Console.WriteLine($"////////////////////////////////////////");
+            Console.WriteLine();
 
+            int temp, min;
+            for (int i = 0; i < myArray.Length; i++)
 
-        //    for (int i = 0; i < myArray.Length; i++)
-        //    {
-        //        for (int j = 0; j < myArray.Length - 1; j++)
-        //        {
-        //            if (myArray[j] > myArray[i] + 1)
-        //            {
-        //                arrayValue = myArray[j];
-        //                myArray[j] = myArray[j + 1];
-        //                myArray[j + 1] = arrayValue;
-        //            }
-        //        }
+            {
+                min = i;
+                for (int j = i + 1; j < myArray.Length; j++)
+                {
 
-        //    }
-        //    for (int i = 0; i < myArray.Length; i++)
-        //    {
-        //        Console.WriteLine(myArray[i]);
-        //    }
+                    if (myArray[j] < myArray[min])
+                    {
+                        min = j;
+                    }
 
-        //    Console.WriteLine();
-        //    Console.WriteLine($"////////////////////////////////////////");
-        //    return myArray;
-        //}
+                }
+
+                temp = myArray[min];
+                myArray[min] = myArray[i];
+                myArray[i] = temp;
+            }
+        
+    
 
 
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
 
-        BubbleSorting();
-        //SelectionSorting();
-        //InsertionSorting();
+            Console.WriteLine();
+            Console.WriteLine($"////////////////////////////////////////");
+            return myArray;
+        }
+
+
+        int[] InsertionSorting()
+        {
+
+            int[] myArray = InputArray();
+
+            Console.WriteLine("\n Sorting array with insertion method  : ");
+            Console.WriteLine($"////////////////////////////////////////");
+            Console.WriteLine();
+
+            int j, min;
+            for (int i = 1; i < myArray.Length; i++)
+
+            {
+                min = myArray[i];
+
+                j = i - 1;
+
+                while (j >= 0 && myArray[j] > min)
+                {
+                    myArray[j + 1] = myArray[j];
+                    j = j - 1;
+                }
+
+                myArray[j + 1] = min;
+
+            }
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"////////////////////////////////////////");
+            return myArray;
+        }
+
+
     }
 
 }
